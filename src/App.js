9cc595092b3628c4f06db468a36cd5450c6645ef
sonhayoni <qheodus123@gmail.com>
2023-06-12 16:34:15 +0900

    add redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ContextApi from './Context/R077_ContextApi'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import StrAddButton from './StrAddButton'
 
-
-function App() {
-  return (
-    <div>
-      <h1>Start React</h1>
-      <ContextApi />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Start React</h1>
+        {/* <span>{this.props.store.getState().data.str}</span><br /> */}
+        <span>{this.props.str}</span><br />
+        {/* <StrAddButton store={this.props.store} /> */}
+        <StrAddButton AppProps="200" />
+      </div>
+    )
+  }
 }
+
+let mapStateToProps = (state, props) => {
+  console.log('Props from index.js : ' + props.indexProp)
+  return {
+    str: state.data.str,
+  };
+};
+
+App = connect(mapStateToProps, null)(App);
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'
+import reducers from './reducers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const CallMiddleware = store => nextMiddle => action => {
+  console.log('1. reducer before progress');
+  console.log('2. action.type: ' + action.type + ', store str : ' + store.getState().data.str);
+  console.log('3. reducer after progress');
+  console.log('4. action.type: ' + action.type + ', store str : ' + store.getState().data.str);
+  return XPathResult;
+}
+
+const store = createStore(reducers, applyMiddleware(CallMiddleware));
+
+const listener = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App indexProp="react" />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+store.subscribe(listener);
+listener();
