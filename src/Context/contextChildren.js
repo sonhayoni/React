8256be6beefa3a0ae64cf9d232cf10e0acd3bcc4
sonhayoni <qheodus123@gmail.com>
2023-06-12 13:57:59 +0900

    add changeContextParent

--- src/Context/contextChildren.js
+++ src/Context/contextChildren.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Children2 from './contextChildren2'
+import { Consumer } from "./R077_ContextApi";
 
 class contextChildren extends React.Component {
     render() {
         return (
-            <Children2 />
+            <Consumer>
+                {contextValue =>
+                    <button onClick={e => contextValue.setStateFunc("react200")}>
+                        {contextValue.name}_button
+                    </button>
+                }
+            </Consumer>
         )
     }
 }
