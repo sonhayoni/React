6e092b3e0cef5df6564bbb6231bc102c86fdce80
sonhayoni <qheodus123@gmail.com>
2023-06-13 13:39:17 +0900

    first commit

---
+++ src/components/R089_reactRouter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class R089_reactRouter extends Component {
+    render() {
+        return (
+            <>
+                <h1>path='/'</h1>
+                <h1>R089_reactRouter</h1>
+            </>
+        )
+    }
+}
+
+export default R089_reactRouter;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
-import reducers from './reducers';
+import App from './components/App';
+import * as serviceWorker from './service-worker';
 
-const CallMiddleware = store => nextMiddle => action => {
-  console.log('1. reducer before progress');
-  console.log('2. action.type: ' + action.type + ', store str : ' + store.getState().data.str);
-  console.log('3. reducer after progress');
-  console.log('4. action.type: ' + action.type + ', store str : ' + store.getState().data.str);
-  return XPathResult;
-}
 
-const store = createStore(reducers, applyMiddleware(CallMiddleware));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
 
-const listener = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App indexProp="react" />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-store.subscribe(listener);
-listener();
+serviceWorker.unregister();
