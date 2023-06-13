7c1daa6dbd957ceba381a000791056e9d1aa1e1f
sonhayoni <qheodus123@gmail.com>
2023-06-13 13:48:51 +0900

    edit

--- src/components/R089_reactRouter.js
+++ src/components/R089_reactRouter.js
@@ -4,7 +4,7 @@ class R089_reactRouter extends Component {
     render() {
         return (
             <>
-                <h1>path='/'</h1>
+                <h1>path='/reactRouter2'</h1>
                 <h1>R089_reactRouter</h1>
             </>
         )
